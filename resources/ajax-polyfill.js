11d4aac8a483d18e00d9889d8e9e235c39e12b3b
Func <Funcer@outlook.com>
2023-09-17 14:44:57 +0800

    fix: Support the fetch API

--- resources/ajax-polyfill.js
+++ resources/ajax-polyfill.js
@@ -1,8 +1,20 @@
-jQuery.ajaxPrefilter( function( options ) {
-	if ( options.crossDomain && jQuery.support.cors ) {
+( function () {
+	function modifyUrl( url ) {
 		var config = require( './config.json' );
-		if ( config.domainList.includes( new URL( options.url ).host ) ) {
-			options.url = config.prefix + options.url;
+		if ( config.domainList.includes( new URL( url ).host ) ) {
+			url = config.prefix + url;
 		}
+		return url;
 	}
-} );
+
+	jQuery.ajaxPrefilter( function( options ) {
+		if ( options.crossDomain && jQuery.support.cors ) {
+			options.url = modifyUrl( options.url );
+		}
+	} );
+
+	var prevFetch = window.fetch;
+	window.fetch = function( url, options ) {
+		return prevFetch.apply( this, [ modifyUrl( url ), options ] );
+	}
+}() );
